d448ad32950864a1e44ade110672fd770a499978
Gatot Triantono <gatottriantono2003@gmail.com>
2023-10-09 08:37:20 +0700

    Aku Gatot

--- app/src/main/java/com/example/fundandro/Fragment.kt
+++ app/src/main/java/com/example/fundandro/Fragment.kt
@@ -1,11 +1,20 @@
 package com.example.fundandro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat
 
 class Fragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+
+
+
     }
 }
